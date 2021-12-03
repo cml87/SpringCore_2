ca6de7b03626d3bd90dd2163cd34b924ada45461
gambarimas87 <gambarimas87@gmail.com>
2021-12-03 12:13:20 +0100

    add Calendar factory bean

--- src/main/java/com/example/conference/AppConfig.java
+++ src/main/java/com/example/conference/AppConfig.java
@@ -4,15 +4,34 @@ import com.example.conference.repository.HibernateSpeakerRepositoryImpl;
 import com.example.conference.repository.SpeakerRepository;
 import com.example.conference.service.SpeakerService;
 import com.example.conference.service.SpeakerServiceImpl;
+import com.example.conference.util.CalendarFactory;
+import java.util.Calendar;
+
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
+import java.util.Calendar;
+
 @Configuration
 @ComponentScan({"com.example.conference"})
 public class AppConfig {
+
+    @Bean("calendarFactory")
+    public CalendarFactory calendarFactory(){
+        CalendarFactory calendarFactory = new CalendarFactory();
+        calendarFactory.addDays(2);
+        return calendarFactory;
+    }
+
+    // We'll get this been from the Spring container, but Spring will create it through
+    // the bean factory 'calendarFactory'
+    @Bean("calendar")
+    public Calendar getCalendar() throws Exception {
+        return calendarFactory().getObject();
+    }
 
     // this return a service configured with a given repository
     // Here we do setter injection. The class where we want to inject the dependency this way must have the
---
+++ src/main/java/com/example/conference/util/CalendarFactory.java
@@ -0,0 +1,25 @@
+package com.example.conference.util;
+
+import org.springframework.beans.factory.FactoryBean;
+
+import java.util.Calendar;
+
+public class CalendarFactory implements FactoryBean<Calendar> {
+
+    private Calendar calendar = Calendar.getInstance();
+
+    @Override
+    public Calendar getObject() throws Exception {
+        return calendar;
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return Calendar.class;
+    }
+
+    public void addDays(int n){
+        calendar.add(Calendar.DAY_OF_YEAR,n);
+    }
+
+}
